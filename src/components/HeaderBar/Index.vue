b6b83808ca7ede734702715a115ad9698e79e107
Andres Nunez <camilonunezz9812@gmail.com>
2022-05-30 15:22:41 -0500

    contact form frontend and validations, changes in camsky references, ready to add emailjs

--- src/components/HeaderBar/Index.vue
+++ src/components/HeaderBar/Index.vue
@@ -8,7 +8,7 @@ import HeaderLinks from './HeaderLinks.vue'
             <div class="navbar-brand">
                 <RouterLink :to="{ name: 'Home' }" class="d-flex">
                     <font-awesome-icon :icon="['fa', 'cube']" :style="{height: '25px'}"></font-awesome-icon>
-                    <h1 class="ms-2 mb-0 fs-4 section__subtitle">camsky</h1>
+                    <h1 class="ms-2 mb-0 fs-4 section__subtitle">Andrés Camilo</h1>
                 </RouterLink>
             </div>
             <button class="navbar-toggler" @click="toggleMenuIcon = !toggleMenuIcon" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- src/components/Home/Contact.vue
+++ src/components/Home/Contact.vue
@@ -6,4 +6,85 @@
         </div>
     </div>
     <h2 class="section__subtitle text-center mt-2">Contact</h2>
+    <div class="row">
+        <form id="first_name contact-form" @submit.prevent="validateContactForm">
+            <div class="row justify-content-center">
+                <div class="form-group mb-3 col-md-8 col-12">
+                    <label class="form-label">Email&nbsp;<span class="monospace">*</span></label>
+                    <input
+                        v-model="contactForm.email"
+                        type="email"
+                        class="form-control contact-form__input input rounded-pill"
+                        :class="{ 'input-error': contactForm.errorEmail }"
+                        @click="() => contactForm.errorEmail = null"
+                        placeholder="Let me know your email"
+                    />
+                    <div v-if="contactForm.errorEmail" class="px-2 mt-2">
+                        <span>{{ contactForm.errorEmail }}</span>
+                    </div>
+                </div>
+                <div class="form-group mb-4 col-md-8 col-12">
+                    <label class="form-label">Message&nbsp;<span class="monospace">*</span></label>
+                    <input
+                        v-model="contactForm.message"
+                        type="text"
+                        class="form-control contact-form__input rounded-pill"
+                        :class="{ 'input-error': contactForm.errorMessage }"
+                        @click="() => contactForm.errorMessage = null"
+                        placeholder="Hi! i'd like to connect"
+                    />
+                    <div v-if="contactForm.errorMessage" class="px-2 mt-2">
+                        <span>{{ contactForm.errorMessage }}</span>
+                    </div>
+                </div>
+                <div class="form-group col-md-8 col-12">
+                    <button type="submit" class="btn px-4 btn--submit rounded-pill">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            contactForm: {
+                email: "",
+                message: "",
+                errorEmail: null,
+                errorMessage: null,
+            }
+        }
+    },
+    methods: {
+        validateContactForm () {
+            if (!this.contactForm.email) {
+                this.contactForm.errorEmail = 'I need a way to answer you, email is mandatory'
+            } else {
+                this.contactForm.errorEmail = null
+            }
+            if (!this.contactForm.message) {
+                this.contactForm.errorMessage = 'Let me know how we can collaborate'
+            } else {
+                this.contactForm.errorMessage = null
+            }
+
+            if (!this.contactForm.errorEmail && !this.contactForm.errorMessage) {
+                this.sendContactEmail()
+            }
+        },
+        sendContactEmail () {
+            const { email, message } = this.contactForm
+            console.log(`Sending email... ${email} ${message}`)
+        }
+    },
+}
+</script>
+
+<style scoped>
+.input-error {
+    border-color: #DC3444;
+    box-shadow: 0 0 0 0.25rem rgb(220 52 68 / 25%);
+}
+</style>
