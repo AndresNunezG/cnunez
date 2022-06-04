879b393227988ab1e94652c23c8323de355f9952
Andres Nunez <camilonunezz9812@gmail.com>
2022-06-04 12:18:40 -0500

    my experience section responsive

--- src/components/Home/JobsList.vue
+++ src/components/Home/JobsList.vue
@@ -12,46 +12,52 @@
         </div>
         <div class="joblist-body col-12">
             <div class="row">
-                <div class="col-4 order-1">
+                <div class="col-md-4 col-12 order-1">
                     <!-- Role -->
-                    <div class="joblist-body__item fw-bold mb-1 d-flex justify-content-end align-items-center">
-                        <span>{{ jobSelected.roleTitle }}</span>
-                        <font-awesome-icon
-                            :icon="['fas', 'bolt']"
-                            :style="{ color: 'var(--green-1)'}"
-                            class="ps-2"
-                        ></font-awesome-icon>
+                    <div class="row mx-0 px-0">
+                        <div class="joblist-body__item fw-bold mx-0 px-0 mb-1 d-flex justify-content-md-end justify-content-start align-items-center">
+                            <span class="order-md-1 order-2">{{ jobSelected.roleTitle }}</span>
+                            <font-awesome-icon
+                                :icon="['fas', 'bolt']"
+                                :style="{ color: 'var(--green-1)'}"
+                                class="order-md-2 order-1 ps-md-2 pe-md-0 pe-2"
+                            ></font-awesome-icon>
+                        </div>
                     </div>
                     <!-- Span -->
-                    <div class="joblist-body__item fw-bold mb-1 d-flex justify-content-end align-items-center">
-                        <p class="m-0">
-                            <span>{{ jobSelected.startDate }}</span>
-                            <span>&nbsp;-&nbsp;</span>
-                            <span>{{ jobSelected.finishData }}</span>
-                        </p>
-                        <font-awesome-icon
-                            :icon="['fas', 'calendar']"
-                            :style="{ color: 'var(--green-1)'}"
-                            class="ps-2"
-                        ></font-awesome-icon>
+                    <div class="row mx-0 px-0">
+                        <div class="joblist-body__item fw-bold mx-0 px-0 mb-1 d-flex justify-content-md-end justify-content-start align-items-center">
+                            <p class="order-md-1 order-2 m-0">
+                                <span>{{ jobSelected.startDate }}</span>
+                                <span>&nbsp;-&nbsp;</span>
+                                <span>{{ jobSelected.finishData }}</span>
+                            </p>
+                            <font-awesome-icon
+                                :icon="['fas', 'calendar']"
+                                :style="{ color: 'var(--green-1)'}"
+                                class="order-md-2 order-1 ps-md-2 pe-md-0 pe-2"
+                            ></font-awesome-icon>
+                        </div>
                     </div>
                     <!-- Website -->
-                    <div class="joblist-body__item fw-bold mb-1 d-flex justify-content-end align-items-center">
-                        <a :href="jobSelected.websiteLink" target="_blank" rel="noopener noreferrer">Website</a>
-                        <font-awesome-icon
-                            :icon="['fas', 'globe']"
-                            :style="{ color: 'var(--green-1)'}"
-                            class="ps-2"
-                        ></font-awesome-icon>
+                    <div class="row mx-0 px-0">
+                        <div class="joblist-body__item fw-bold mx-0 px-0 mb-1 d-flex justify-content-md-end justify-content-start align-items-center">
+                            <a class="order-md-1 order-2" :href="jobSelected.websiteLink" target="_blank" rel="noopener noreferrer">Website</a>
+                            <font-awesome-icon
+                                :icon="['fas', 'globe']"
+                                :style="{ color: 'var(--green-1)'}"
+                                class="order-md-2 order-1 ps-md-2 pe-md-0 pe-2"
+                            ></font-awesome-icon>
+                        </div>
                     </div>
                 </div>
                 <!-- Role Description -->
-                <div class="col-6 order-2 border-start">
+                <div class="col-md-6 col-12 order-2 text-left">
                     <p
                         v-for="(roleDescriptionItem, index) in jobSelected.roleDescription"
                         :key="index"
                     >
-                    <span>{{ roleDescriptionItem }}</span>
+                    <span>{{ index + 1 }}.&nbsp;{{ roleDescriptionItem }}</span>
                     </p>
                 </div>
             </div>
